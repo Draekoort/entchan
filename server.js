d6d6ae2427413f4f223030b050f19b853978252d
draekoort <draekoort@outlook.com>
2024-08-05 21:11:04 +0200

    Préparation pour le déploiement sur Render

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
